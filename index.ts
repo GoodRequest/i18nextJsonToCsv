323160d9dc6d24df801249e64dde76b54d6d98d8
HaluskaRoman <roman.haluska@goodrequest.com>
2022-04-28 12:47:16 +0200

    fixing issue

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 #!/usr/bin/env ts-node
-import convertTranslationsFromJSONToCSV from './scripts/translationsJSONToCSV'
-import convertTranslationsFromCSVToJSON from './scripts/translationsCSVToJSON'
+// import convertTranslationsFromJSONToCSV from './scripts/translationsJSONToCSV'
+// import convertTranslationsFromCSVToJSON from './scripts/translationsCSVToJSON'
 
 // load config file
-    const configFile = require(`${ process.cwd() }${ process.env.CONFIGPATH ? process.env.CONFIGPATH : './config.json' }`)
+const configFile = require(`${ process.cwd() }${ process.env.CONFIGPATH ? process.env.CONFIGPATH : './config.json' }`)
 
-    if(process.env.JSONToCSV
-)
-{
-    convertTranslationsFromJSONToCSV(configFile)
+if(process.env.JSONToCSV) {
+    // convertTranslationsFromJSONToCSV(configFile)
 }
 
 if (process.env.CSVToJSON) {
-    convertTranslationsFromCSVToJSON(configFile)
+   // convertTranslationsFromCSVToJSON(configFile)
 }
