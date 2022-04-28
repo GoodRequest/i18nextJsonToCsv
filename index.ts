2b09387a25dfe4103bce8ab263f6ac5709bb5b5d
HaluskaRoman <roman.haluska@goodrequest.com>
2022-04-28 12:37:37 +0200

    fixing issue

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 import convertTranslationsFromJSONToCSV from './scripts/translationsJSONToCSV'
 import convertTranslationsFromCSVToJSON from './scripts/translationsCSVToJSON'
 
